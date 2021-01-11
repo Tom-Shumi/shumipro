80b6e35479ddec24ff24ca6b2c59b5bd307845eb
Tomohide Shumiya <shumiya@TomohidenoMacBook-Pro.local>
2021-01-11 13:49:45 +0900

    tomcat起動用に修正

--- src/main/java/com/ne/jp/shumipro/ShumiProjectApplication.java
+++ src/main/java/com/ne/jp/shumipro/ShumiProjectApplication.java
@@ -2,12 +2,18 @@ package com.ne.jp.shumipro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ShumiProjectApplication {
+public class ShumiProjectApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShumiProjectApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ShumiProjectApplication.class);
+	}
 }
